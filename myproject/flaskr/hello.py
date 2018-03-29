f10a70a5ffffcfad21df5b58c13db460b1d23c52
wenming0310 <474196608@qq.com>
2018-03-30 00:00:53 +0800

    4c

--- myproject/flaskr/hello.py
+++ myproject/flaskr/hello.py
@@ -1,24 +1,23 @@
-from flask import Flask, render_template, session, redirect, url_for
-from flask import make_response
-from flask import redirect
-from flask import abort
-from flask.ext.script import Manager
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from datetime import datetime
-from flask.ext.wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-
-class NameForm(Form):
-    name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SECRET_KEY'] = 'None'
+#app.config['SECRET_KEY'] = 'hard to guess string'
+#app.config['WTF_CSRF_ENABLED'] = 'False'
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 #manager = Manager(app)
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 # 把业务逻辑和表现逻辑混在一起会导致代码难以理解和维护
 """
@@ -76,6 +75,17 @@ def index():
         form.name.data = ''
     return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
 '''
+#重新定向
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name!=form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -83,15 +93,15 @@ def user(name):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'),500
+    return render_template('500.html'), 500
 
 #D:\CC++C#Code
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
     #使用Flask-Script
     #manager.run()
