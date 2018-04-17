2746028d7fb07679ce22908f9e6c8cf4e2c40dce
wenming0310 <474196608@qq.com>
2018-04-18 00:15:11 +0800

    8e error

--- myproject/Eric's Blogs/app/auth/views.py
+++ myproject/Eric's Blogs/app/auth/views.py
@@ -54,3 +54,22 @@ def confirm(token):
     if current_user.confirm(token):
         flash('You have confirmation link is invalid or has expired.')
     return redirect(url_for('main.index'))
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated() and not current_user.confirmed and request.endpoint[:5]!='auth.':#and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymouus() or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/uunconfirmed.html')
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account', 'auth/email/comfirm', user=current_user, token=token)
+    flash('A nee confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
