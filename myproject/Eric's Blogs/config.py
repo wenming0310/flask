89089a0b457b3f3d34196ca3b8b7a3da39011f6e
wenming0310 <474196608@qq.com>
2018-04-21 00:41:04 +0800

    2018年4月21日00:40:44
    邮件发送成功，验证功能待确认

--- myproject/Eric's Blogs/config.py
+++ myproject/Eric's Blogs/config.py
@@ -8,10 +8,10 @@ class Config:
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '25'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
         ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'zhaowenming0310@sina.com')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'zhaowenming')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'zhaowenming0310@sina.com>'
+    FLASKY_MAIL_SENDER = 'zhaowenming<zhaowenming0310@sina.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
