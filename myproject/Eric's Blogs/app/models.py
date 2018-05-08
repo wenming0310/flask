3f0ddf9f5d9906298e5bde3eb2f00df1e28dc2fd
zhaowenming <474196608@qq.com>
2018-05-08 19:43:10 +0800

    1

--- myproject/Eric's Blogs/app/models.py
+++ myproject/Eric's Blogs/app/models.py
@@ -4,6 +4,8 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
+import hashlib
+from flask import  request
 
 
 class Permission:
@@ -165,6 +167,16 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating
+        )
 
     def __repr__(self):
         return '<User %r>' % self.username
